f84cd23568dee0bc7586635453df361830b76775
Akshat30 <akshat30@gmail.com>
2024-09-07 11:57:53 -0700

    Set up dev branch to work on main website

--- app/page.tsx
+++ app/page.tsx
@@ -9,38 +9,39 @@ import { redirect } from 'next/navigation';
 
 export default async function Index() {
   const supabase = createClient();
-  return redirect('/interest');
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser();
-  // if (user) {
-  //   return redirect('/interest');
-  // }
 
-  // return (
-  //   <div className="flex-1 w-full flex flex-col gap-20 items-center">
-  //     <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-  //       <div className="flex flex-col gap-8 sm:gap-16 items-center">
-  //         <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center mt-32">
-  //           Welcome to the application portal!
-  //         </p>
-  //         <p className="text-2xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-  //           Please sign in.
-  //         </p>
-  //         <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-  //       </div>
-  //       <main className="flex flex-col justify-center items-center">
-  //         <GoogleOAuth />
-  //         <div className="text-gray-400 text-xs mt-4">
-  //           *When signing in, it will ask to continue to{' '}
-  //           <span className="font-bold">kvuilkasrtgyazkvxjal.supabase.co</span>
-  //           <p className="text-gray-500 mt-4">
-  //             If you're having any issues or have any questions, please contact
-  //             AJ/Christen @ 6507720579/5103345535
-  //           </p>
-  //         </div>
-  //       </main>
-  //     </div>
-  //   </div>
-  // );
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user) {
+    return redirect('/dashboard');
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+        <div className="flex flex-col gap-8 sm:gap-16 items-center">
+          <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center mt-32">
+            Welcome to the Fall 2024 Rush!
+          </p>
+          {/* {travis add button to go to interest form here} */}
+          <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+        </div>
+        <main className="flex flex-col justify-center items-center">
+          <p className="text-xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center mb-4">
+            Please sign in for the application.
+          </p>
+          <GoogleOAuth />
+          <div className="text-gray-400 text-xs mt-4">
+            *When signing in, it will ask to continue to{' '}
+            <span className="font-bold">kvuilkasrtgyazkvxjal.supabase.co</span>
+            <p className="text-gray-500 mt-4">
+              If you're having any issues or have any questions, please contact
+              Ally or Val @ (916) 841-7952 / (408) 805-2888!
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
