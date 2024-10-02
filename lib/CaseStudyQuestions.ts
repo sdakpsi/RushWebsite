296710976535cb450ab4ca08b4c935ba346df9e7
Akshat30 <akshat30@gmail.com>
2024-10-02 14:28:34 -0700

    changed case questions

--- lib/CaseStudyQuestions.ts
+++ lib/CaseStudyQuestions.ts
@@ -1,26 +1,26 @@
 export const caseStudyData = [
-    {
-      name: "Leadership",
-      label: "leadership",
-    },
-    {
-        name: "Teamwork",
-        label: "teamwork",
-    },
-    {
-        name: "Public Speaking",
-        label: "publicSpeaking",  
-    },
-    {
-        name: "Analytical",
-        label: "analytical",
-    },
-    {
-        name: "What role did they believe they played?",
-        label: "role",
-    },
-    {
-        name: "What did they think of the case study? What did they did like/not like about it? ",
-        label: "thoughts"
-    }
-]
+  {
+    name: "Leadership",
+    label: "leadership",
+  },
+  {
+    name: "Teamwork",
+    label: "teamwork",
+  },
+  {
+    name: "Public Speaking",
+    label: "publicSpeaking",
+  },
+  {
+    name: "Analytical",
+    label: "analytical",
+  },
+  {
+    name: "How do you think the group performed as a whole, and how did you contribute individually?",
+    label: "role",
+  },
+  {
+    name: "What did you like and dislike about the case study, and why?",
+    label: "thoughts",
+  },
+];
