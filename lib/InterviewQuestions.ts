1b92524703f38f6125e12abb8731f7faa8e065f8
Akshat30 <akshat30@gmail.com>
2024-10-05 00:05:52 -0700

    interview form is modified

--- lib/InterviewQuestions.ts
+++ lib/InterviewQuestions.ts
@@ -1,96 +1,97 @@
 export const questions = [
   {
-    label: '1. Tell me about yourself in 1 minute.',
-    name: 'aboutYourself',
-    note: '(1 min) Look for relevant answers to their relationship with friends/family. Also look for interesting information about their hobbies. Ask follow-up questions that reveal what they learned from leadership positions. Ask where they live currently!',
+    label: "1. Tell me about yourself in 1 minute.",
+    name: "aboutYourself",
+    note: "(1 min) Look for relevant answers to their relationship with friends/family. Also look for interesting information about their hobbies. Ask follow-up questions that reveal what they learned from leadership positions. Ask where they live currently!",
   },
   {
     label:
-      '2. What are your career interests and where do you see yourself in 5 and 10 years?',
-    name: 'careerInterests',
-    note: '(2 mins) Are they ambitious and have long term goals? Have they taken the time to figure out what steps they need to take in their forsee-able future?',
+      "2. What are your career interests and ideal professional roles? Why?",
+    name: "careerInterests",
+    note: "(2 mins) Are they ambitious and have long term goals? Have they taken the time to figure out what steps they need to take in their forsee-able future?",
   },
   {
     label:
-      '3. What is an instance that you went out of your way to do something for a friend?',
-    name: 'instanceForFriend',
-    note: '(2 minutes) Look for examples of sacrifice here !!!',
+      "3. What is an instance that you went out of your way to do something for a friend?",
+    name: "instanceForFriend",
+    note: "(2 minutes) Look for examples of sacrifice here !!!",
   },
   {
     label:
-      '4. Tell me about a failure of yours and how you were able to overcome it.',
-    name: 'failureOvercome',
+      "4. Tell me about a failure of yours and how you were able to overcome it.",
+    name: "failureOvercome",
     note: "(2 mins) Look for examples of failure that involve others in the scenario. If they don't reflect on coming to terms with their failure, dig a little deeper.",
   },
   {
     label:
-      '5. Can you tell me about a time when you had a disagreement with a teammate and how you handled the situation?',
-    name: 'disagreementHandled',
-    note: '(2 mins) Dig deep to see what the conflict was, how they handled it, and how they reflect on the situation as a whole!',
-  },
-  {
-    label: '6. How do you handle criticism? Please give an example.',
-    name: 'handlingCriticism',
-    note: '(2 mins) pls pls press with questions/have them elaborate if they say they handle criticism poorly',
+      "5. Can you tell me about a time when you had a disagreement with a teammate and how you handled the situation?",
+    name: "disagreementHandled",
+    note: "(2 mins) Dig deep to see what the conflict was, how they handled it, and how they reflect on the situation as a whole!",
   },
   {
     label:
-      "7. What is something that you've been meaning to learn about but haven't got to yet? How would you go about doing that/what is your plan?",
-    name: 'learningAbout',
-    note: '(2 mins) Shows how they want to learn and be curious! Also try to dig deep how they take action about this.',
+      "6. Can you share a time you received tough criticism? What did you learn from it, and what was the result?",
+    name: "handlingCriticism",
+    note: "(2 mins) Pls Pls press with questions/have them elaborate if they say they handle criticism poorly. This is our chance to see how they might take program critiques!",
+  },
+  {
+    label: "7. What is an achievement that you are proud of?",
+    name: "learningAbout",
+    note: "(2 mins) Very open ended, goal is to give them the floor to showcase something unique or impressive about them.",
   },
   {
     label:
-      '8. Silly Questions: If you had to describe your personality as a kitchen item, what would you choose? ●  Explain one of your uncommon opinions. ● Who would you want to play you in a movie? ● Who would you want to be your fictional sidekick?  ● What is your comfort food? ● You’ve been given an elephant. You can’t give it away or sell it. What would you do with the elephant? ● If you were an ice cream flavor which one would you be and why? How would you sell ice cream in Alaska?',
-    name: 'sillyQuestion',
-    note: 'Choose ONE.',
+      "8. Silly Questions: If you had to describe your personality as a kitchen item, what would you choose? ●  Explain one of your uncommon opinions. ● Who would you want to play you in a movie? ● Who would you want to be your fictional sidekick?  ● What is your comfort food? ● You’ve been given an elephant. You can’t give it away or sell it. What would you do with the elephant? ● If you were an ice cream flavor which one would you be and why? How would you sell ice cream in Alaska?",
+    name: "sillyQuestion",
+    note: "Choose ONE.",
   },
   {
     label:
-      '9. Do you have any questions about the pledging process? (Give them time to answer before moving on) It is expected of all members to complete this pledging process, which takes up the time equivalent of a 4-unit class. If you are given an invitation to join AKPsi, would you be able to commit to weekly meetings on Thursday evenings (past 8pm) and Sunday afternoons? (*pause for answer) What other time commitments do you have this quarter? (If they are a red flag, follow the red flag doc) ',
-    name: 'questionsAndCommitments',
-  },
-  {
-    label: '10. Why should we give you a bid? ',
-    name: 'whyGiveBid',
-    note: '(1 min) Actives... try to see if they take this opportunity to give it their all, to take this second chance at improving their interview.. we want kids who take all chances they get',
+      "9. Do you have any questions about the pledging process? (Give them time to answer before moving on) It is expected of all members to complete this pledging process, which takes up the time equivalent of a 4-unit class. If you are given an invitation to join AKPsi, would you be able to commit to weekly meetings on Thursday evenings (past 8pm) and Sunday afternoons? (*pause for answer) What other time commitments do you have this quarter? (If they are a red flag, follow the red flag doc) ",
+    name: "questionsAndCommitments",
   },
   {
     label:
-      '11. Who was the most influential active AKPsi member you met or talked to this week? Why?',
-    name: 'mostInfluential',
-    note: '(1 min) No alumni members! If they mention a member, please pay attention to why they chose them; if they had a conversation about family, friendship, etc., with another member.. pls take note of it! If they connected to a story in this example, they value family!',
+      "10. Why should we give you a bid? What do you hope to gain from AKPsi?",
+    name: "whyGiveBid",
+    note: "(1 min) Actives... try to see if they take this opportunity to give it their all. we want kids who believe they belong here. Note what they personally want to gain from AKPsi too.",
   },
   {
-    label: '12. Do you have any more questions for us?',
-    name: 'moreQuestions',
+    label:
+      "11. Who was the most influential active AKPsi member you met or talked to this week? Why?",
+    name: "mostInfluential",
+    note: "(1 min) No alumni members! If they mention a member, please pay attention to why they chose them; if they had a conversation about family, friendship, etc., with another member.. pls take note of it! If they connected to a story in this example, they value family!",
+  },
+  {
+    label: "12. Do you have any more questions for us?",
+    name: "moreQuestions",
   },
 ];
 
 export const scorableTraits = [
   {
-    displayName: 'Empathy',
-    propertyName: 'empathy',
-    note: 'Are they kind and respectful? Are they willing to sacrifice for their brothers?',
+    displayName: "Empathy",
+    propertyName: "empathy",
+    note: "Are they kind and respectful? Are they willing to sacrifice for their brothers?",
   },
   {
-    displayName: 'Open-mindedness',
-    propertyName: 'openmindedness',
-    note: 'Have they pushed their comfort zones and involved themselves in different experiences (sports/orgs/etc). Are they willing to make mistakes and learn from them? Do you see them receptive to the challenges of pledging?',
+    displayName: "Open-mindedness",
+    propertyName: "openmindedness",
+    note: "Have they pushed their comfort zones and involved themselves in different experiences (sports/orgs/etc). Are they willing to make mistakes and learn from them? Do you see them receptive to the challenges of pledging?",
   },
   {
-    displayName: 'Pledgeable',
-    propertyName: 'pledgeable',
-    note: 'How down are they to join AKP? Is this something they are committed to and have done research for? Did they seem prepared for the interview? ( They can be nervous and shy but prepared more in terms of their answers ) Do they seem down to be pledged?',
+    displayName: "Downbad for AKPsi",
+    propertyName: "pledgeable",
+    note: "How down are they to join AKP? Is this something they are committed to and have done research for? Did they seem prepared for the interview? ( They can be nervous and shy but prepared more in terms of their answers )",
   },
   {
-    displayName: 'Motivated',
-    propertyName: 'motivated',
+    displayName: "Motivated",
+    propertyName: "motivated",
     note: "Do they have a lot of passions/interests that they like to talk about? Are they passionate about the things in their lives? What else do they want to learn that they haven't got an opportunity to do yet?",
   },
   {
-    displayName: 'Socially Aware',
-    propertyName: 'sociallyAware',
-    note: 'Were they overall respectful/nice in the interview? Did they have subjectively good morals and not say anything sus or weird?',
+    displayName: "Socially Aware",
+    propertyName: "sociallyAware",
+    note: "Were they overall respectful/nice in the interview? Did they have subjectively good morals and not say anything sus or weird?",
   },
 ];
