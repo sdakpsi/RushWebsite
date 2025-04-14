1e8a4c9f784a4e632e826c31dcb6038d46befb71
Akshat30 <akshat30@gmail.com>
2025-04-14 06:48:53 -0700

    fix for bigs

--- components/ApplicantCard.tsx
+++ components/ApplicantCard.tsx
@@ -146,7 +146,7 @@ const ApplicantCard: React.FC<ApplicantCardProps> = ({
             </p>
           </div>
         </div>
-        <div className="mt-2 text-left">
+        {/* <div className="mt-2 text-left">
           {numCaseStudies >= 3 ? (
             <p className="text-xs text-green-500">
               {numCaseStudies} Cases: {caseActives.join(", ")}
@@ -168,7 +168,7 @@ const ApplicantCard: React.FC<ApplicantCardProps> = ({
         </div>
         <div className="mt-2">
           <span>Total Score: {total}</span>
-        </div>
+        </div> */}
       </div>
     </button>
   );
