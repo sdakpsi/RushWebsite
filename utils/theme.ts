402bd757deca7fc08e9789be12b73d2a89e5a4ab
itsgeorgema <georgema2020@gmail.com>
2026-03-16 00:54:31 -0700

    remove the trade secret lol

--- utils/theme.ts
+++ utils/theme.ts
@@ -64,7 +64,7 @@ export const currentTheme: Theme = {
     rushYear: "Fall '25",
     organization: "UCSD Alpha Kappa Psi",
     website: "https://www.akpsiucsd.com",
-    rushChairs: "contact Amanda Wu or Joshua Hoffmann @ (949)-572-8948 / (510)-634-9132",
+    rushChairs: "contact our rush chair Josh Hoffmann @ (510)-634-9132",
     applicationOpen: false,
   }
 };
