626b4c071d33ad14df6c652f4486f1c9a2bcd708
yathinm <yathinmrudul@gmail.com>
2026-09-13 21:22:16 -0700

    update: interest form

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer() {
               <Link href="/terms" className="text-muted-foreground hover:text-primary transition-colors">
                 Terms of Service
               </Link>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLScbhQ7B1qzs5_ZWkjY-RFDejC34dG3di7dMbu_xFA4pe33KYA/viewform" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSdJYewWBkiy7ryWcwB5a617X8uvAwhsMndle5C3pDKCZ_h_Pw/viewform" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
                 Interest Form
               </a>
             </div>
